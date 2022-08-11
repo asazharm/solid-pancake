3185836ea09cccaf66179778dc3d50e31a741ef2
Zhora Asatryan <as.zh.mi.27@gmail.com>
2022-08-11 18:26:01 +0400

    Banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -10,12 +10,14 @@ export default function Banner() {
         <div className={c['banner__content__desc']}>Устанавливаются в общественных местах для предотвращения распространения вирусов микробов</div>
       </div>
 
-      <div className={c[['banner__btn']]}>
-        <button>
-          <img src="images/arrow.svg" alt="" />
-          Подробнее
+      {/* <div > */}
+        <button className={c[['banner__btn']]}>
+          <div>
+            <img src="images/arrow.svg" alt="" />
+          </div>
+          <p>Подробнее</p>
         </button>
-      </div>
+      {/* </div> */}
       <div className={c['banner__img']}>
         <img src="images/banner_product.png" alt="product"/>
       </div>
